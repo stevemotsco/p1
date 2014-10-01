515ff45c5c1b505626fb9744d586998d074d98c5
Steve Motsco <stephenmotsco@g.harvard.edu>
2014-09-30 22:18:54 -0500

    Changes made based on pointers from TA after grading.

--- index.php
+++ index.php
@@ -33,36 +33,36 @@
 
 	<div class="container">
 	
-		<h1><center>Steve Motsco <br>CSCI E-15: Dynamic Web Applications Portfolio</h1>
+		<h1>Steve Motsco <br/>CSCI E-15: Dynamic Web Applications Portfolio</h1>
 		
 		<img src='images/USMC-Huey-Anbar,Iraq-2008.jpg' alt='Steve Motsco'>
 		
 		
 
-		<h3>Project 2 - PHP Basics</h3>
-		<p>View: [FutureLink]<br>Github: [FutureLink]</p>
+		<h2>Project 2 - PHP Basics</h2>
+		<h6>View: [FutureLink]<br/>Github: [FutureLink]</h6>
 		
-		<h3>Project 3 - Laravel Basics</h3>
-		<p>View: [FutureLink]<br>Github: [FutureLink]</p>
+		<h2>Project 3 - Laravel Basics</h2>
+		<h6>View: [FutureLink]<br/>Github: [FutureLink]</h6>
 		
-		<h3>Project 4 - Final</h3>
-		<p>View: [FutureLink]<br>Github: [FutureLink]</p>
+		<h2>Project 4 - Final</h2>
+		<h6>View: [FutureLink]<br/>Github: [FutureLink]</h6>
 
 		
-		<h2>In 2+ sentences describe who you are and what you do.</h2>
+		<h3>In 2+ sentences describe who you are and what you do.</h3>
 		<p>At the end of the day I am just a country boy who loves living on the horse farm with my beautiful bride of 24 years.  We currently have 18 horses which my wife uses to take people on guided trail rides through the state forest which butts up to our property in the panhandle of Florida.  Our two sons are both currently majoring in computer science at the University of West Florida.  I am part of a small group of software engineers who maintain software applications and databases that are used for obsolescence management by the military, other government entities, and businesses in the civilian sector.  Additionally, I have been wearing a U.S. Marine Corps uniform on and off since 1982 which has taken me to more countries than I can easily count on five continent (many times flying the helicopter pictured above).  I have seen many wonderful things and many horrific things, but the one thing I know for sure I have learned is though the United States is far from perfect, it is by far the best game in town.</p>
 		
-		<h2>Why are you taking this course?</h2>
+		<h3>Why are you taking this course?</h3>
 		<p>I am taking this course because in the past two years at work our software group has transformed three of our desktop applications into web based applications.  This was done by our four most junior programmers while two other programmers and I continued to add enhancements to the desktop applications and the 40+ databases that support them.  I wish to be significantly involved with migrating these enhancements to the web based applications, so I decided to take some courses in building web applications to augment what I have learned by trial and error in the past six months.</p>
 		
-		<h2>What previous experience do you have with programming?</h2>
+		<h3>What previous experience do you have with programming?</h3>
 		<p>The first computer programs I wrote were in 1980 written in Fortarn 77 for a mainframe using punch cards.  It has been a long road since then and I have used more programming languages than I care to remember.  Over the years I have spent a good amount of my time working on databases and programming in T-SQL.</p>
 		
-		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+		<h3>Are you working on a Mac, PC, or some other platform?</h3>
 		<p>I currently use two PCs, a laptop and a desktop, for the programming and other tasks I do at work.  I also use these computers for my schoolwork since I do not personally own a computer.</p>
 		
-		<h2>Anything else we should know?</h2>
-		<p>Someday when they make the stone, all it will have on it is:<br>NAME<br>START DATE-END DATE<br>“Husband, Father, Patriot”</p>
+		<h3>Anything else we should know?</h3>
+		<p>Someday when they make the stone, all it will have on it is:<br/>NAME<br/>START DATE-END DATE<br>“Husband, Father, Patriot”</p>
 	
 	</div>
 	
